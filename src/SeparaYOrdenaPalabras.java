21d463de94f4ae3d6c38697c368c43b6b5fa8e51
Andrei Cimpoeru <andrei.cimpoeru@bahiasoftware.es>
2023-01-10 19:15:35 +0100

    6.20 - Bianca Milea

--- src/SeparaYOrdenaPalabras.java
+++ src/SeparaYOrdenaPalabras.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class SeparaYOrdenaPalabras {
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Scanner;
+
+public class SeparaYOrdenaPalabras {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Introduce una frase: ");
+        String frase = scanner.nextLine();
+
+        List<String> palabras = Arrays.asList(frase.split(" "));
+        palabras.sort(Comparator.comparing(String::toLowerCase));
+
+        System.out.println("Palabras ordenadas: ");
+        for (String palabra : palabras) {
+            System.out.println(palabra);
+        }
+    }
 }
